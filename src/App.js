0feda7f6521b99f4389a96ae5908b4d70d08779e
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2025-02-11 16:29:49 +0100

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navBar/Navbar';
 
 function App() {
   return (
     <div className='App'>
-      <p>Portfolio</p>
+      <Navbar />
     </div>
   );
 }
