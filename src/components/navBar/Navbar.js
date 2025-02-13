b9665ccb6d58ad761e744083dff2a0684a1918ed
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2025-02-13 17:43:37 +0100

    deploy in githubpages

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -1,50 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/logo4.png';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div>
-      <nav className='navbar navbar-expand-lg navbar-dark  custom-navbar'>
-        <a className='navbar-brand' href=''>
+      <nav className='navbar navbar-expand-lg navbar-dark custom-navbar'>
+        <Link className='navbar-brand' to='/'>
+          {' '}
+          {/* Usar Link para el logo también */}
           <img className='logo' src={logo} alt='logo' />
-        </a>
+        </Link>
         <button
           className='navbar-toggler'
           type='button'
-          data-toggle='collapse'
-          data-target='#navbarSupportedContent'
+          onClick={toggleMenu} // Controlar el toggle con React
           aria-controls='navbarSupportedContent'
-          aria-expanded='false'
+          aria-expanded={isMenuOpen} // Enlazar aria-expanded al estado
           aria-label='Toggle navigation'
         >
           <span className='navbar-toggler-icon'></span>
         </button>
 
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+        <div
+          className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} // Clase condicional
+          id='navbarSupportedContent'
+        >
           <ul className='navbar-nav ms-auto'>
-            <li className='nav-item active'>
-              <Link to={'/'}>
-                <a className='nav-link'>
-                  Inicio <span className='sr-only'></span>
-                </a>
-              </Link>
-            </li>
             <li className='nav-item'>
-              <Link to={'/about'}>
-                <a className='nav-link'>Sobre mi</a>
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link to={'/portfolio'}>
+              {' '}
+              {/* Quitar la clase 'active' a menos que sea necesario */}
+              <Link to='/' className='nav-link' onClick={toggleMenu}>
                 {' '}
-                <a className='nav-link'>portfolio</a>
+                {/* Cerrar al hacer clic */}
+                Inicio
               </Link>
             </li>
             <li className='nav-item'>
-              <Link to={'/contact'}>
-                {' '}
-                <a className='nav-link'>contacto</a>
+              <Link to='/about' className='nav-link' onClick={toggleMenu}>
+                Sobre mi
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/portfolio' className='nav-link' onClick={toggleMenu}>
+                Portfolio
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/contact' className='nav-link' onClick={toggleMenu}>
+                Contacto
               </Link>
             </li>
           </ul>
