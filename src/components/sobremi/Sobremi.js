8cf7cf9c9126995fc86677fc93766082ce5c9be8
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2025-07-15 11:17:19 +0200

    adjunto nuevo cv

--- src/components/sobremi/Sobremi.js
+++ src/components/sobremi/Sobremi.js
@@ -13,7 +13,8 @@ import logoSpring from '../../assets/springboot.png';
 import logoBootstrap from '../../assets/bootstrap.png';
 export default function Sobremi() {
   const cvLink =
-    'https://drive.google.com/uc?export=download&id=1e7t844fM5UXFh8mI3ZsrnAhmKKSWJZZ7';
+    // 'https://drive.google.com/uc?export=download&id=1e7t844fM5UXFh8mI3ZsrnAhmKKSWJZZ7';
+    'https://drive.google.com/file/d/1aYQ34nzYwl185hmPGrRrvAiD5fQBykJu/view?usp=drive_link';
   return (
     <div className='container'>
       <Container className='my-5 container'>
