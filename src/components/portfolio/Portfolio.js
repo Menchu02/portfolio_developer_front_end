1c5b737ba47f91a59e2901432f027c5d91dbd38b
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2026-02-24 19:54:45 +0100

    adjunto nueva card a portfolio

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -5,15 +5,16 @@ import photoCard2 from '../../assets/photo-jesus.png';
 import photoCard3 from '../../assets/photo-sandalia.png';
 import photoCard4 from '../../assets/shopping.png';
 import photoCard5 from '../../assets/laravel_foto.jpg';
+import photoCard6 from '../../assets/portfolio2.png';
 
 export default function Portfolio() {
   const projects = [
     {
       id: 1,
-      title: 'Portfolio fotografía',
+      title: 'Portfolio fotografía Jesús Coll',
       description:
         'Sitio web de fotografía para destacar la obra del artista, con galerías dinámicas.',
-      tags: ['Front-end', 'Responsive'],
+      tags: ['Front-end', 'React'],
       imageUrl: photoCard2,
       altText: 'Portfolio fotografía',
       link: 'https://jesuscollgallardo.com',
@@ -21,10 +22,10 @@ export default function Portfolio() {
     },
     {
       id: 2,
-      title: 'Movie App',
+      title: 'App de películas',
       description:
         'App web de películas consumiendo API: crear, editar y eliminar contenido.',
-      tags: ['Front-end', 'API'],
+      tags: ['Front-end', 'React', 'API'],
       imageUrl: photoCard1,
       altText: 'Movie App',
       link: 'https://menchu02.github.io/movie-app-main/',
@@ -34,7 +35,7 @@ export default function Portfolio() {
       id: 3,
       title: 'Laravel CRUD',
       description:
-        'CRUD con filtros, compra vía email y generación de reunión (Google Meet).',
+        'CRUD de posts,filtra,elimina, edita, compra vía email y genera reunión (Google Meet).',
       tags: ['Back-end', 'Laravel'],
       imageUrl: photoCard5,
       altText: 'Laravel CRUD',
@@ -43,10 +44,10 @@ export default function Portfolio() {
     },
     {
       id: 4,
-      title: 'Shopping Cart',
+      title: 'Carrito de compra',
       description:
         'Carrito de compras para tienda online: añade productos y gestiona cantidades.',
-      tags: ['Front-end', 'Estado'],
+      tags: ['Front-end', 'React'],
       imageUrl: photoCard3,
       altText: 'Shopping Cart',
       link: 'https://menchu02.github.io/shopping-cart/',
@@ -54,14 +55,24 @@ export default function Portfolio() {
     },
     {
       id: 5,
-      title: 'Shopping List',
-      description:
-        'Lista de la compra: añade, edita y elimina productos de forma rápida.',
-      tags: ['Front-end', 'CRUD'],
+      title: 'Lista de la compra',
+      description: 'Lista de la compra: añade, edita y elimina productos.',
+      tags: ['Front-end', 'React'],
       imageUrl: photoCard4,
       altText: 'Shopping List',
       link: 'https://menchu02.github.io/shopping-list/',
       linkGithub: 'https://github.com/Menchu02/shopping-list',
+    },
+    {
+      id: 6,
+      title: 'Portfolio Carmen Blanco',
+      description:
+        'Mi portfolio personal donde expongo quién soy y algunos de mis trabajos.',
+      tags: ['Front-end', 'React'],
+      imageUrl: photoCard6,
+      altText: 'Portfolio ',
+      link: 'https://menchu02.github.io/portfolio_developer_front_end/',
+      linkGithub: 'https://github.com/Menchu02/portfolio_developer_front_end',
     },
   ];
 
